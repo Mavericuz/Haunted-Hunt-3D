a1269fb855a57db459e83b70d51b9e1a358f8712
Maverick <dittobitz17@gmail.com>
2020-04-30 13:30:26 +0530

    EndGame
    exit UI created to end the game

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsplayerAtExit;
+    float m_Timer;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject == player)
+        {
+            m_IsplayerAtExit = true;
+        }
+    }
+    void Update()
+    {
+        if(m_IsplayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
